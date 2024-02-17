407e902505c297b35583030a2c2391f240c577bb
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-02-17 13:06:13 -0300

    fit: start develope de style of home page

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -1,9 +1,15 @@
 import Browser  from "../components/browser";
+import img from "../style/img/Cypher_img_1.png"
 
 function Home(){
     return(
         <div>
             <Browser/>
+            <br/>
+            <div className="greetingsImg">
+                <img src={img} alt="greeting img"/>
+            </div>
+            
         </div>
     )
 }
