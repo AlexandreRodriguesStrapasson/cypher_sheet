7c036044c3c82b60e3b9ecbc64b70f25bc2f9509
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-02-17 12:25:30 -0300

    fit: add react-router-dom

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainBody">
+        <div>
+          <ul>
+            <li><button>Opção 1</button></li>
+            <li><button>Opção 2</button></li>
+            <li><button>Opção 3</button></li>
+            <li><button>Opção 4</button></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
