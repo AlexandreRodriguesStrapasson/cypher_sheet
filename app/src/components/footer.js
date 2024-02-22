5aa246a2da5f2109f167d96dfe15067b79280dd7
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-02-21 21:05:23 -0300

    feat: add the footer

---
+++ app/src/components/footer.js
@@ -0,0 +1,13 @@
+import Style from "../style/imgHome.module.css"
+
+function footer(){
+    return(
+        <div className={Style.footer}> 
+            <p>Todas as informações, imagens e demais conteudos são de origem e propriedade do Cypher System</p>
+            <p>Para mais informações acesse o site</p>
+            <p><a href="https://cypher-system.com/">Cypher System</a></p>
+        </div>       
+    )
+}
+
+export default footer;
--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -1,21 +1,21 @@
-import Browser  from "../components/browser";
 import img from "../style/img/Cypher_img_1.png"
 import Style from "../style/imgHome.module.css"
+import Footer from "../components/footer"
 
 function Home(){
     return(
         <div>
-            <Browser/>
             <br/>
             <div className="container">
                 <img src={img} alt="greeting img" className={Style.center} />
                 <div className={Style.message}>
-                    <h1>Bem Vindo Viajnate</h1>
+                    <h1>Bem Vindo Viajante</h1>
                     <h2>A</h2>
                     <h1>Forja de herois</h1>
+                    <button className={Style.btn}>Forja</button>
                 </div>
-            </div>
-            
+            </div>  
+            <Footer/>
         </div>
     )
 }
