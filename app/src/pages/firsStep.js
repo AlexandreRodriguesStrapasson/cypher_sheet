08969f84062479b3f46e2e4f0fab46212a110c40
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-02-27 20:25:19 -0300

    feat: add fields to names and descriptions

--- app/src/pages/firsStep.js
+++ app/src/pages/firsStep.js
@@ -3,7 +3,31 @@ import Style from "../style/firstStep.module.css"
 function firsStep (){
     return(
         <div>
-            <h1 className={Style}>Página das primeiros configs</h1>
+            <div className={Style.mainBody}>
+                <div>
+                    <p>Para começar, vamos dar um nome para o seu herói, descreve-lo e estabalecer seu backGround</p>
+                    <label>Nome do jogador</label>
+                    <input type="text"></input>
+                    
+                    <label>Nome do Personajem</label>
+                    <input type="text"></input>
+
+                    <br/>
+                    <label>Descrição: </label>
+                    <br/>
+                    <textarea  placeholder="Cabelos negros e longos, alcançando a altura dos ombros; Olhos azuis e penetrantes; Alto com corpo robusto"/>
+                    <br/>
+                    
+                    
+                    <label>História: </label>
+                    <br/>
+                    <textarea  placeholder="Nasci em um vilarejo pequeno, meu pai era um caçador e minha mãe uma padeira. Cresci sonhando com grandes aventruas e com as maravilhas escondidas pelo mundo, quando me tornei um adulto decidi viajar para entender mais sobre esse mundo e sobre mim mesmo"/>
+                    <br/>
+
+                    <button>Próximo passo</button>
+
+                </div>
+            </div>
         </div>
     )
 }
