808edb81b8177b8af129487955a99a807faaad9f
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-02-27 19:40:43 -0300

    feat: developed the initial configs to the next page

---
+++ app/src/pages/firsStep.js
@@ -0,0 +1,12 @@
+import Style from "../style/firstStep.module.css"
+
+function firsStep (){
+    return(
+        <div>
+            <h1 className={Style}>Página das primeiros configs</h1>
+        </div>
+    )
+}
+
+
+export default firsStep;
