15517dc2f827e0c2134a7ee66047dcde68ced622
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-03-05 20:09:35 -0300

    feat: add initial model of description

--- app/src/pages/thirdStep.js
+++ app/src/pages/thirdStep.js
@@ -1,42 +1,56 @@
 import React, { useState } from 'react';
 import Style from "../style/thirdStep.module.css"
 
-function ThirdStep(){
+function ThirdStep() {
     const [selectedOption, setSelectedOption] = useState(''); 
     const handleSelectChange = (event) => {
         setSelectedOption(event.target.value);
     };
 
     const getDescription = (option) => {
-        const descriptions = {
-            'Opção 1': 'Foco 1',
-            'Opção 2': 'Foco 2',
-            'Opção 3': 'Foco 3',
-            'Opção 4': 'Foco 4',
-            'Opção 5': 'Foco 5',
-            'Opção 6': 'Foco 6',
-        };
-
-        return descriptions[option] || '';
+        switch (option) {
+            case 'Permance na Pedra':
+                return (
+                    <>
+                        <p>Sua carne é feita de mineral duro, fazendo de você um humanoide corpulento e difícil de ferir.</p>
+                        <ul>
+                            <li>Tier 1: Corpo de Golem</li>
+                            <li>Tier 1: Cura Golem</li>
+                            <li>Tier 2: Aperto Golem</li>
+                        </ul>
+                    </>
+                );
+            case 'Opção 2':
+                return (
+                    <>
+                        <p>Foco 2 </p>
+                    </>
+                );
+            case 'Opção 3':
+                return (
+                    <>
+                        <p>Foco 3</p>
+                    </>
+                );
+            default:
+                return '';
+        }
     };
 
-
-    return(
+    return (
         <div className={Style.mainBody}>
             <h1>Agora é hora de escolher o foco do personagem</h1>
-                <select onChange={handleSelectChange}>
-                    <option value="">Descritor</option>
-                    <option value="Opção 1">Opção 1</option>
-                    <option value="Opção 2">Opção 2</option>
-                    <option value="Opção 3">Opção 3</option>
-                    <option value="Opção 4">Opção 4</option>
-                    <option value="Opção 5">Opção 5</option>
-                    <option value="Opção 6">Opção 6</option>
+            <select onChange={handleSelectChange}>
+                <option value="">Selecione um foco...</option>
+                <option value="Permance na Pedra">Permance na Pedra</option>
+                <option value="Opção 2">Opção 2</option>
+                <option value="Opção 3">Opção 3</option>
             </select>
-            <p>{getDescription(selectedOption)}</p>
+            <div>
+                {getDescription(selectedOption)}
+            </div>
         </div>
-    )
+    );
 }
-
 
 export default ThirdStep;
