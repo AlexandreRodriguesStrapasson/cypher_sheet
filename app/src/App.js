e3fc682d747e8fcfae414e666fd857afd4ea4b73
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-03-07 18:21:30 -0300

    feat: add the sheet page

--- app/src/App.js
+++ app/src/App.js
@@ -4,15 +4,17 @@ import Home from "./pages/home"
 import FirsSteps from './pages/firsStep';
 import SecondStep from './pages/secondStep';
 import ThirdStep from './pages/thirdStep';
+import Sheet from './pages/sheet';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
-        <Route path='/firstStep' element={<FirsSteps/>}></Route>
-        <Route path='/secondStep' element={<SecondStep/>}></Route>
+        <Route path='/FirstStep' element={<FirsSteps/>}></Route>
+        <Route path='/SecondStep' element={<SecondStep/>}></Route>
         <Route path='/ThirdStep' element={<ThirdStep/>}></Route>
+        <Route path='/Sheet' element={<Sheet/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ app/src/pages/sheet.js
@@ -0,0 +1,11 @@
+import Style from "../style/sheet.module.css";
+
+function Sheet(){
+    return(
+        <div className={Style.mainBody}>
+            <h1>Pagina da Ficha finalizada</h1>
+        </div>
+    )
+}
+
+export default Sheet;
