5fdcc3fb901f3419cb0b637963eef3f15a738ddc
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-03-07 19:43:10 -0300

    style: add a initial desing to the div "characterInfo"

--- app/src/pages/sheet.js
+++ app/src/pages/sheet.js
@@ -4,15 +4,17 @@ function Sheet(){
     return(
         <div className={Style.mainBody}>
             <h2>Ficha Cypher</h2>
-            <div className="carachterInfo">
+            <div className={Style.characterInfo}>
                 <input value="Nome do jogador" readOnly/>
                 <input value="Nome do personagem" readOnly/>
                 <input value="Descritor" readOnly/>
                 <input value="Tipo" readOnly/>
                 <input value="foco" readOnly/>
-
             </div>
-            <div className={Style.attributeBox}></div>
+
+            <div className={Style.attributeBox}>
+                <h3>Atributos</h3>
+            </div>
             <div className="skills"></div>
             <div className="attack"></div>
             <div className="cyphers"></div>
