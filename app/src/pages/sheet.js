243df83d5a0f75798541f5d3786d64d208848e21
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-03-07 19:36:52 -0300

    feat: add a model of the fields that going to be filled by Data base

--- app/src/pages/sheet.js
+++ app/src/pages/sheet.js
@@ -3,7 +3,24 @@ import Style from "../style/sheet.module.css";
 function Sheet(){
     return(
         <div className={Style.mainBody}>
+            <h2>Ficha Cypher</h2>
+            <div className="carachterInfo">
+                <input value="Nome do jogador" readOnly/>
+                <input value="Nome do personagem" readOnly/>
+                <input value="Descritor" readOnly/>
+                <input value="Tipo" readOnly/>
+                <input value="foco" readOnly/>
+
+            </div>
             <div className={Style.attributeBox}></div>
+            <div className="skills"></div>
+            <div className="attack"></div>
+            <div className="cyphers"></div>
+            <div className="equipament"></div>
+
+            <div className="abilitties"></div>
+            <div className="backGround"></div>
+            <div className="notes"></div>
         </div>
     )
 }
