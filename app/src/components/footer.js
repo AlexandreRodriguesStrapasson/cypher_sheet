9f04eeaf6e93b7f3a2fdfa30c72ca9451550d7e1
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-04-28 22:31:14 -0300

    style: add new comment in the home

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -3,8 +3,9 @@ import Style from "../style/imgHome.module.css"
 function footer(){
     return(
         <div className={Style.footer}> 
+            <p>Projeto para fins didáticos</p>
             <p>Todas as informações, imagens e demais conteudos são de origem e propriedade do Cypher System</p>
-            <p>Para mais informações acesse o site</p>
+            <p>Para mais informações acesse o site</p>            
             <p><a href="https://cypher-system.com/">Cypher System</a></p>
         </div>       
     )
